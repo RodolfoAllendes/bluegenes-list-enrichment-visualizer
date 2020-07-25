f13e833c6dbadc425b763a938eb00ad08eedfa65
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-07-25 15:14:02 +0530

    tested widget data with some mock options

--- src/RootContainer.js
+++ src/RootContainer.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import queryData from './query';
 
-class RootContainer extends React.Component {
-	render() {
-		return (
-			<div className="rootContainer">
-				<h1>Your Data Viz Here</h1>
-			</div>
-		);
-	}
-}
+const RootContainer = ({ serviceUrl, entity }) => {
+	const [filterOptions, setFilterOptions] = useState({
+		maxp: 0.05,
+		processFilter: 'biological_process',
+		correction: 'Holm-Bonferroni',
+		limitResults: 20,
+		widget: 'go_enrichment_for_gene'
+	});
+
+	useEffect(() => {
+		queryData({
+			serviceUrl,
+			geneIds: entity.value,
+			filterOptions
+		})
+			.then(res => {
+				console.log(res);
+			})
+			.catch(() => {
+				console.log('No Enrichment data found!');
+			});
+	}, [filterOptions]);
+	return (
+		<div className="rootContainer">
+			<h1>Your Data Viz Here</h1>
+		</div>
+	);
+};
 
 export default RootContainer;
