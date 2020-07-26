284e0feedbff851ab7caedc8c016f89c25998cd8
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-07-26 23:02:30 +0530

    set response into state and added loading

--- src/RootContainer.js
+++ src/RootContainer.js
@@ -2,30 +2,40 @@ import React, { useEffect, useState } from 'react';
 import queryData from './query';
 
 const RootContainer = ({ serviceUrl, entity }) => {
-	const [filterOptions, setFilterOptions] = useState({
+	const filterOptions = {
 		maxp: 0.05,
 		processFilter: 'biological_process',
 		correction: 'Holm-Bonferroni',
 		limitResults: 20,
 		widget: 'go_enrichment_for_gene'
-	});
+	};
+	const [data, setData] = useState([]);
+	const [loading, setLoading] = useState(false);
 
 	useEffect(() => {
+		setLoading(true);
 		queryData({
 			serviceUrl,
 			geneIds: entity.value,
 			filterOptions
 		})
 			.then(res => {
-				console.log(res);
+				setLoading(false);
+				setData(res.slice(0, filterOptions.limitResults));
 			})
 			.catch(() => {
-				console.log('No Enrichment data found!');
+				setLoading(false);
 			});
-	}, [filterOptions]);
+	}, []);
 	return (
 		<div className="rootContainer">
-			<h1>Your Data Viz Here</h1>
+			{loading ? (
+				<h1>Loading...</h1>
+			) : data.length ? (
+				<div></div>
+			) : (
+				<h1>No Enrichment data found!</h1>
+			)}
 		</div>
 	);
 };
