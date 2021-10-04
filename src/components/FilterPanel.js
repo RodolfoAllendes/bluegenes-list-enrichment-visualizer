4b1a4791bd9fe76378a07478c6862f34b3ffe744
Rodolfo Allendes <rallendes@nibiohn.go.jp>
2021-10-04 10:01:45 +0900

    Added test correction select.

--- src/components/FilterPanel.js
+++ src/components/FilterPanel.js
@@ -1,13 +1,32 @@
 import React from 'react';
-// , { useEffect, useState }
+// import { useEffect, useState } from 'react';
 
 // Definition of the FilterPanel Component
 // data
 const FilterPanel = () => {
+	const correctionValues = [
+		'Holm-Bonferroni',
+		'Benjamini Hochberg',
+		'Bonferroni'
+	];
+
 	return (
 		<div className="filter-panel">
 			<div className="filter-container">
 				<h5 className="report-item-heading">Widget Filters</h5>
+				<p>Test Correction:</p>
+				<div className="control">
+					<select name="correction">
+						{correctionValues.map(c => (
+							<option value={c} key={c}>
+								{c}
+							</option>
+						))}
+					</select>
+				</div>
+				<p>Max p-value:</p>
+				<p>Filter</p>
+				<button className="apply-filters-button">Apply</button>
 			</div>
 		</div>
 	);
